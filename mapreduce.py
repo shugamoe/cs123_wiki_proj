7930d4225e5e9042c54add5ec88567e7995fcdf4
Bobby Adusumilli <badusumilli@cs.uchicago.edu>
2016-05-05 20:52:12 -0500

    Map Reduce should work

--- mapreduce.py
+++ mapreduce.py
@@ -1,6 +1,13 @@
-# To run code: python3 mapreduce.py pagecounts-20090826-200000
+# To run code and save output of reducers to plain text documents: 
+# If 'mrjob_output' folder already exists, it gets overwritten
+# python3 mapreduce.py -o 'mrjob_output' --no-output pagecounts-20090825-210000
 
-# pip3 install --user mr3px.csvprotocol
+# To run code on all files in a bucket in EMR, running on 1 computer: 
+# python mr_wordcount.py --num-ec2-instances=1 --python-archive package.tar.gz -r emr -o 's3://dataiap-bobbyadusumilli-testbucket/output' --no-output 's3://dataiap-wikipedia/*'
+
+# Really good for AWS Map Reduce: https://dataiap.github.io/dataiap/day5/mapreduce
+
+# MRJob documentation: https://media.readthedocs.org/pdf/mrjob/latest/mrjob.pdf
 
 import mrjob
 from mrjob.job import MRJob
@@ -8,6 +15,10 @@ import re
 import os
 import datetime
 import json
+import gzip
+
+
+# Figure out how to decompress files within MRJob
 
 
 class EnglishEntries(MRJob):
@@ -16,9 +27,10 @@ class EnglishEntries(MRJob):
 	total views for each page. 
 	'''
 	# To not display null values in the final output
-	OUTPUT_PROTOCOL = mrjob.protocol.JSONProtocol
+	# INPUT_PROTOCOL = mrjob.protocol.
+	OUTPUT_PROTOCOL = mrjob.protocol.JSONValueProtocol
 
-	def mapper(self, _, line):
+	def mapper(self, _, file):
 		'''
 		Starter function that will take each line of the space-separated 
 		file and yield each English Wikipedia's page name and the number 
@@ -32,15 +44,20 @@ class EnglishEntries(MRJob):
 			Total views of that Wikipedia page
 		'''
 		# fields[0] = language, fields[1] = page name, fields[2] = page views, fields[3] = bytes
-		fields = line.split(' ')
-		filename = os.environ["mapreduce_map_input_file"]
-		date = re.findall("[0-9]+-[0-9]+", filename)[0]
-		date = datetime.datetime.strptime(date, "%Y%m%d-%H%M%S")
-		date = datetime.datetime.strftime(date, "%Y/%m/%d/%H")
+		with gzip.open(file, 'rb') as f: 
+			file_content = f.read()
+			file_content = str(file_content).split("\\n")
 
-		# Only care about entries in English
-		if re.findall('en', fields[0]) != []:
-			yield fields[1] + " " + date, int(fields[2])
+		for line in file_content: 
+			fields = line.split(' ')
+			filename = os.environ["mapreduce_map_input_file"]
+			date = re.findall("[0-9]+-[0-9]+", filename)[0]
+			date = datetime.datetime.strptime(date, "%Y%m%d-%H%M%S")
+			date = datetime.datetime.strftime(date, "%Y/%m/%d/%H")
+
+			# Only care about entries in English
+			if re.findall('en', fields[0]) != []:
+				yield fields[1] + " " + date, int(fields[2])
 
 	def combiner(self, page_name, views):
 		'''
@@ -70,12 +87,9 @@ class EnglishEntries(MRJob):
 			Wikipedia page name
 			Total views of that Wikipedia page
 		'''
-		# print(os.environ["mapreduce_map_input_file"])
-		if re.findall("avatar", page_name[0].lower()) != []:
-			if re.findall("File:", page_name[0]) == []: 
-				with open('mrjob_output.txt', 'w') as outfile: 
-					json.dump(page_name + " " + sum(views), outfile)
-				# yield (page_name, sum(views))
+		if re.findall("avatar", page_name.lower()) != []:
+			if re.findall("File:", page_name) == []: 
+				yield None, page_name + " " + str(sum(views))
 
 if __name__ == '__main__':
 	EnglishEntries.run()
