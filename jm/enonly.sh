f799ec5497846a7cce0599a955d8c8c971f3097b
Julian McClellan <jmcclellan@cs.uchicago.edu>
2016-05-13 23:59:38 -0500

    Heyo

--- jm/enonly.sh
+++ jm/enonly.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 
-read -p "Enter hour from 00 to 23 <HH>:" HOURS
+read -p "Enter hour from 000 to 230 <HH0>:" HOURS
 
-for f in *-$HOURS00*; do
+for f in *-$HOURS*; do
     echo Filtering $f
     sed -i '/^en /!d' $f
 done
--- jm/unzip.sh
+++ jm/unzip.sh
@@ -3,11 +3,12 @@
 for f in *.gz; do
     STEM=$(basename "${f}" .gz)
     if [ -e ~/wikitrafv2big/pagecounts/"${STEM}" ]; then
+        echo "${STEM}" already exists, skipping file.
+    else 
         echo "${STEM}" not yet decompressed... Decompressing.
         sudo gunzip -c "${f}" > ~/wikitrafv2big/pagecounts/"${STEM}"
-    else 
-        echo "${STEM}" already exists, skipping file.
-    fi 
+    fi
+done
 
 
 
