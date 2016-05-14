7d0d12aad484a480c35e7c33e803568e342e81eb
Julian McClellan <jmcclellan@cs.uchicago.edu>
2016-05-14 00:13:17 -0500

    Heyo

--- jm/mount_s3.sh
+++ jm/mount_s3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
+read -p "Enter Key" KEY 
+read -p "Enter Secret Key" SKEY
+
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install fuse fuse-devel autoconf automake curl-devel libxml2-devel openssl-devel mailcap
 wget https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.78.tar.gz
@@ -10,7 +13,7 @@ cd s3fs-fuse-1.78
 make
 sudo make install
 cd
-echo "<key>:<s_key>" > .passwd-s3fs
+echo "$KEY:$SKEY" > .passwd-s3fs
 chmod 600 .passwd-s3fs 
 mkdir s3_wiki
 s3fs wikitrafv2big  s3_wiki
