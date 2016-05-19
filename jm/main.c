11c6335a0198c141037554977edf540c61ca0d7c
Julian McClellan <jmcclellan@cs.uchicago.edu>
2016-05-19 13:42:41 -0500

    lab stuff

--- jm/main.c
+++ jm/main.c
@@ -17,56 +17,74 @@ bool done = false;
 
 void* producer(void* unused) {
     printf("Producer Start\n");
-    // waitFor(2);
     int i;
-    printf("Creating products\n");
     for (i = 0; i < 500; i++) {
         /* produce something */
         /* append it to a list */
+        pthread_mutex_lock(&m);
+        // printf("Locked_p\n");
         c++;
+        printf("+1");
+        pthread_cond_signal(&cond_var);
+        // printf("Signal Sent\n");
+        pthread_mutex_unlock(&m);
+        // printf("Unlocked_p\n");
     }
-    printf("Now %d products\n", c);
     pthread_mutex_lock(&m);
-    printf("Locked_p\n");
-    done = true;
-    pthread_cond_signal(&cond_var);
-    printf("Signal Sent\n");
-    pthread_mutex_unlock(&m);
-    printf("Unlocked_p\n");
+    done = true; 
+    printf("\tProduction Complete\n");
+    pthread_cond_broadcast(&cond_var);  
+    pthread_mutex_unlock(&m); 
     return NULL;
 }
 
+
 void* consumer(void* unused) {
     printf("Consumer start\n");
-    // waitFor(2);
     pthread_mutex_lock(&m);
-    printf("Locked_c\n");
     while (!done) {
         printf("Checking\n");
         pthread_cond_wait(&cond_var, &m);
+        while (c > 0) {
+        /* remove something from list */
+            c--;
+            printf("-1");
+            pthread_mutex_unlock(&m);
+            pthread_mutex_lock(&m);
+        }
+        pthread_mutex_unlock(&m);
+        pthread_mutex_lock(&m);
     }
-    printf("Will now consume products\n");
+    pthread_mutex_unlock(&m);
     while (c > 0) {
         /* remove something from list */
         c--;
     }
-    pthread_mutex_unlock(&m);
-    printf("Unlocked_c\n");
     return NULL;
 }
 
+
 int main(int argc, char** argv) {
-    pthread_t prod, con;
+    int num_con = atoi(argv[1]);
+    pthread_t* con = (pthread_t*)malloc(sizeof(pthread_t)*num_con); 
+    pthread_t prod;
+    int i;
 
     printf("Creating Threads\n");
     pthread_create(&prod, NULL, producer, NULL);
-    pthread_create(&con, NULL, consumer, NULL);
+    for (i = 1; i <= num_con; i++) {
+        pthread_create(&con[i], NULL, consumer, NULL);
+    }
+    
 
     void* unused;
-    printf("Joining threads\n");
     pthread_join(prod, &unused);
-    pthread_join(con, &unused);
-    printf("Threads Finished\n");
 
+    for (i = 1; i <= num_con; i++) {
+        void* noneed;
+        pthread_join(con[i], &noneed);
+    }
+    printf("Threads Finished\n");
+    free(con);
     printf("Net: %d\n", c);
 }
