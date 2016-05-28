3838732458f7268e17b6b0324be9d9c73537ecd8
Bobby Adusumilli <badusumilli@cs.uchicago.edu>
2016-05-28 13:03:22 -0500

    Allow to work

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import json
+# from pathLib import Path
 
 
 def two_inlinks_sample(json_file_one_to_five):
@@ -23,9 +24,10 @@ def two_inlinks_sample(json_file_one_to_five):
     return two_inlinks_sample
 
 
-def create_relevant(file):
+def create_relevant(file, json_links):
     '''
     file example: 'one-to-five/part-00000'
+    json_links example: 'one_to_five_inlinks.txt'
 
     Function to update file to get the appropriate MRJob data for the 
     regression analysis
@@ -34,6 +36,9 @@ def create_relevant(file):
     homepage and datetimes that are in the data, and also the the 
     information of the appropriate links
     '''
+
+    # mrjob_outputs = [ptx_as_posix() for pth in Path.cwd().iterDir() if 'part-' in pth.stem]
+
     # Create a list of all infromation initial MRJob_Step2 output
     inlinks_mrjob = []
     with open(file, 'r') as f:
@@ -41,20 +46,28 @@ def create_relevant(file):
             inlinks_mrjob.append(line.strip()[1:-1])
 
     # Call function to get all homepages and links of interest
-    inlinks_dict = two_inlinks_sample('one_to_five_inlinks.txt')
+    inlinks_dict = two_inlinks_sample(json_links)
+    # print(inlinks_dict)
+    # print()
 
     inlinks_dict_keys = list(inlinks_dict.keys())
+    # print(inlinks_dict_keys)
+    # print()
 
     # Create a dictionary of homepages and datetimes in MRJob_Step2 data
     keys = {}
     # Create a list of relevant output for the MRJob_Step2 file
     final_lines = []
+    new_inlinks_dict = {}
 
     for line in inlinks_mrjob:
         # fields = [pagename, datetime, pageviews, bytes]
         fields = line.split('   ')
         if fields[0] in inlinks_dict_keys:
             keys[fields[0]] = keys.get(fields[0], []) + [fields[1]]
+            new_inlinks_dict[fields[0]] = new_inlinks_dict.get(fields[0], [])
+
+    # print(list(keys.keys()))
 
     for line in inlinks_mrjob:
         # fields = [pagename, datetime, pageviews, bytes]
@@ -70,6 +83,8 @@ def create_relevant(file):
                 if fields[0] in inlinks_dict[key]:
                     if fields[1] in keys[key]:
                         final_lines.append(line)
+                        if fields[0] not in new_inlinks_dict[key]:
+                            new_inlinks_dict[key] = new_inlinks_dict[key] + [fields[0]]
                         break
 
     # Overwrite the initial MRJob_Step2 file
@@ -77,3 +92,4 @@ def create_relevant(file):
         for line in final_lines: 
             f.write(line + "\n")
 
+    return new_inlinks_dict
