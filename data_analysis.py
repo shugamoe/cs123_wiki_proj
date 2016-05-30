c26e08d820a53b9283a4f78b28bfe227f13eea29
Andrew Tse Zhu <andyz422@cs.uchicago.edu>
2016-05-29 19:09:13 -0500

    hi

--- data_analysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('sample_data/pagecounts-20090826-210000', delimiter = ' ', \
-	columns = list('page name', 'datetime', 'page views'))
-
